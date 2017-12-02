8c9d450c288327c60ed7cc1dd426654a6bc4c541
Federico <tacchinifederico@gmail.com>
2017-12-02 13:31:38 +0100

    Cast to type docu

--- src/helper/cast-parameter-types.ts
+++ src/helper/cast-parameter-types.ts
@@ -1,3 +1,8 @@
+/**
+ * This decorator is required to expose method metadata,
+ * making casting posible when no other attributes in a middleware
+ * handler are annotated.
+ */
 export const CastParameterTypes = function (): any {
     return function (): any {
 
