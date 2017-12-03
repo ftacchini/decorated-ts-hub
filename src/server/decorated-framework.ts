f2ba913a2d09518492b426bce90ebbeda23e36e8
Federico <tacchinifederico@gmail.com>
2017-12-03 23:50:16 +0100

    improving Decorated framework bootstraping

--- src/server/decorated-framework.ts
+++ src/server/decorated-framework.ts
@@ -5,7 +5,11 @@ import { interfaces } from "inversify";
 
 export class DecoratedFramework implements TsFramework {
 
-    constructor(private container: HubContainer) {
+    private constructor(private container: HubContainer) {
+    }
+
+    public static bootstrap(container: HubContainer) : DecoratedFramework {
+        return new DecoratedFramework(container);
     }
 
     public setupFramework(): ControllerLoader{ 
@@ -45,24 +49,44 @@ export class DecoratedFramework implements TsFramework {
     get controllerLoader(): ControllerLoader {
         return this._controllerLoader || (this._controllerLoader = new MetadataControllerLoader());
     }
+    public withContollerLoader(metadataControllerLoader: MetadataControllerLoader) : this {
+        this._controllerLoader = metadataControllerLoader;
+        return this;
+    }
 
     private _routeReader: RouteReader;
     get routeReader(): RouteReader {
         return this._routeReader || (this._routeReader = new MetadataRouteReader(this.container));
+    }
+    public withRouteReader(routeReader: MetadataRouteReader) : this {
+        this._routeReader = routeReader;
+        return this;
     }
     
     private _middlewareReader: MiddlewareReader;
     get middlewareReader(): MiddlewareReader {
         return this._middlewareReader || (this._middlewareReader = new MetadataMiddlewareReader(this.container));
     }
+    public withMiddlewareReader(middlewareReader: MetadataMiddlewareReader) : this {
+        this._middlewareReader = middlewareReader;
+        return this;
+    }
     
     private _functionReader: FunctionReader;
     get functionReader(): FunctionReader {
         return this._functionReader || (this._functionReader = new MetadataFunctionReader(this.container));
     }
+    public withFunctionReader(functionReader: MetadataFunctionReader) : this {
+        this._functionReader = functionReader;
+        return this;
+    }
     
     private _paramsReader: ParameterReader;
     get paramsReader(): ParameterReader {
         return this._paramsReader || (this._paramsReader = new MetadataParameterReader(this.container));
+    }
+    public withParameterReader(paramsReader: MetadataParameterReader) : this {
+        this._paramsReader = paramsReader;
+        return this;
     }
 }
